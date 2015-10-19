9c1e4225f1d3b63dfefa975922c587e5df021744
Stephan <klarsichtweise@gmail.com>
2015-10-19 16:18:59 +0200

    Add Header Templates
    Added all files from the template group "Header Templates"
    No changes have been made to the files

---
+++ templates/Header Templates/header_menu_calendar.php
@@ -0,0 +1,1 @@
+<li><a href="{$mybb->settings['bburl']}/calendar.php" class="calendar">{$lang->toplinks_calendar}</a></li>
---
+++ templates/Header Templates/header_welcomeblock_member.php
@@ -0,0 +1,20 @@
+<!-- Continuation of div(class="upper") as opened in the header template -->
+	<span class="welcome">{$lang->welcome_back} <a href="{$mybb->settings['bburl']}/member.php?action=logout&amp;logoutkey={$mybb->user['logoutkey']}" class="logout">{$lang->welcome_logout}</a></span>
+	</div>
+</div>
+<div class="lower">
+	<div class="wrapper">
+		<ul class="menu panel_links">
+			<li><a href="{$mybb->settings['bburl']}/usercp.php" class="usercp">{$lang->welcome_usercp}</a></li>
+			{$modcplink}
+			{$admincplink}
+		</ul>
+		<ul class="menu user_links">
+			<li><a href="#" onclick="MyBB.popupWindow('{$mybb->settings['bburl']}/misc.php?action=buddypopup&modal=1', null, true); return false;">{$lang->welcome_open_buddy_list}</a></li>
+			<li><a href="{$mybb->settings['bburl']}/search.php?action=getnew">{$lang->welcome_newposts}</a></li>
+			<li><a href="{$mybb->settings['bburl']}/search.php?action=getdaily">{$lang->welcome_todaysposts}</a></li>
+			<li><a href="{$mybb->settings['bburl']}/private.php">{$lang->welcome_pms}</a> {$lang->welcome_pms_usage}</li>
+		</ul>
+	</div>
+	<br class="clear" />
+</div>
